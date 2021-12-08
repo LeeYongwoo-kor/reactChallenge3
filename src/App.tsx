1292c66d6eea5f4a85dcba1c05b750955351b708
LeeYongwoo-kor <newlyo1205@naver.com>
2021-12-08 13:51:12 +0900

    study ts(interface/props/state)

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Circle from "./Circle"
+
+function App() {
+  return (
+    <div>
+      <Circle bgColor="teal" borderColor="blue" />
+      <Circle bgColor="tomato" text="I'm here" />
+    </div>
+  );
+}
+
+export default App;
